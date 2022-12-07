ea740e617bca27621e0f383c454547775e03dcc8
Larissa Perinoto <perinotolarissa@gmail.com>
2022-12-07 11:39:48 -0300

    Altera mensagens e status de erro.

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -4,7 +4,7 @@ const login = async (credentials) => {
   const { email, password } = credentials;
   const user = await User.findOne({ where: { email, password } });
 
-  if (!user) return { status: 500,  message: 'Email ou password incorretos!' };
+  if (!user) return { status: 401,  message: 'Email ou senha incorretos!' };
 
   return { status: 200,  message: 'Usuário logado' }
 };
